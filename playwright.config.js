c141a9b9b4e357ed09ec8dc88420674932e1689f
Bertrand DELION <bertrand.delion@free.fr>
2025-06-30 14:31:54 +0200

    npm run test:e2e OK

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,24 @@
-module.exports = {
+// playwright.config.js
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  testDir: 'e2e',
+  timeout: 30 * 1000,
+  retries: 1,
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   use: {
     headless: true,
-    baseURL: 'http://localhost:8080',
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    baseURL: 'http://localhost:8080'
   },
+  webServer: {
+    command: 'npm run preview',
+    port: 8080,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI
+  }
 };
+
+module.exports = config;
