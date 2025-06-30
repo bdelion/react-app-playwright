5eb36bb33670b42c43740fbe4d45134ff0f4d16b
Bertrand DELION <bertrand.delion@free.fr>
2025-06-30 14:56:30 +0200

    Sonar coverage Jest + Playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,21 @@
 // playwright.config.js
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-  testDir: 'e2e',
-  timeout: 30 * 1000,
-  retries: 1,
-  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
+  testDir: 'e2e',            // Dossier où se trouvent les tests E2E Playwright
+  timeout: 30 * 1000,        // Timeout global par test (30 secondes)
+  retries: 1,                // Nombre de retry en cas d’échec
+  reporter: [
+    ['list'],
+    ['junit', { outputFile: 'playwright-report/junit.xml' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8080',
   },
   webServer: {
     command: 'npm run preview',
